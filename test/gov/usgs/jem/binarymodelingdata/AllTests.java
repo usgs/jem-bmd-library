f21adb5c75f28b75b9a1d5fabe6a82a8f6c83749
Mark McKelvy <mckelvym@usgs.gov>
2016-08-18 14:49:24 -0400

    Added AllTests suite

---
+++ test/gov/usgs/jem/binarymodelingdata/AllTests.java
@@ -0,0 +1,58 @@
+package gov.usgs.jem.binarymodelingdata;
+
+import org.apache.log4j.BasicConfigurator;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Tests all cases.
+ *
+ * @author mckelvym
+ * @since Aug 18, 2016
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ BMDHeaderTest.class })
+public final class AllTests
+{
+	/**
+	 * Ensure that a testing class has defined all required testing methods.
+	 *
+	 * @param p_ClassToTest
+	 *            the class that is being tested
+	 * @param p_TestingClass
+	 *            the class that is doing the testing
+	 * @since Jul 28, 2015
+	 * @since Mar 16, 2016
+	 */
+	public static void assertHasRequiredMethods(final Class<?> p_ClassToTest,
+			final Class<?> p_TestingClass)
+	{
+		try
+		{
+			setUpBeforeClass();
+		}
+		catch (final Exception e)
+		{
+			e.printStackTrace();
+			Assert.fail("Unable to set up tests: " + e.getMessage());
+		}
+		try
+		{
+			Tests.assertHasRequiredMethods(p_ClassToTest, p_TestingClass);
+		}
+		catch (final NoSuchMethodException e)
+		{
+			Assert.fail(e.getMessage());
+		}
+	}
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception
+	{
+		BasicConfigurator.configure();
+	}
+}
