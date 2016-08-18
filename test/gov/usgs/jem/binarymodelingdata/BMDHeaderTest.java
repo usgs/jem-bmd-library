63b3c3f3305e6e9245de11fb283fd90148dc1168
Mark McKelvy <mckelvym@usgs.gov>
2016-08-18 14:44:18 -0400

    Added stub BMDHeaderTest.

---
+++ test/gov/usgs/jem/binarymodelingdata/BMDHeaderTest.java
@@ -0,0 +1,174 @@
+package gov.usgs.jem.binarymodelingdata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * TODO Describe
+ * 
+ * @author mckelvym
+ * @since Aug 18, 2016
+ *
+ */
+public class BMDHeaderTest
+{
+
+	/**
+	 * TODO
+	 * @throws java.lang.Exception
+	 * @since Aug 18, 2016
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception
+	{
+	}
+
+	/**
+	 * TODO
+	 * @throws java.lang.Exception
+	 * @since Aug 18, 2016
+	 */
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	/**
+	 * Test method for {@link gov.usgs.jem.binarymodelingdata.BMDHeader#builder()}.
+	 */
+	@Test
+	public final void testBuilder()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link gov.usgs.jem.binarymodelingdata.BMDHeader#getEndTime()}.
+	 */
+	@Test
+	public final void testGetEndTime()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link gov.usgs.jem.binarymodelingdata.BMDHeader#getOldSeedTime()}.
+	 */
+	@Test
+	public final void testGetOldSeedTime()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link gov.usgs.jem.binarymodelingdata.BMDHeader#getProducer()}.
+	 */
+	@Test
+	public final void testGetProducer()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link gov.usgs.jem.binarymodelingdata.BMDHeader#getSeedJDay()}.
+	 */
+	@Test
+	public final void testGetSeedJDay()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link gov.usgs.jem.binarymodelingdata.BMDHeader#getSeedSecond()}.
+	 */
+	@Test
+	public final void testGetSeedSecond()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link gov.usgs.jem.binarymodelingdata.BMDHeader#getSegmentsSize()}.
+	 */
+	@Test
+	public final void testGetSegmentsSize()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link gov.usgs.jem.binarymodelingdata.BMDHeader#getSignature()}.
+	 */
+	@Test
+	public final void testGetSignature()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link gov.usgs.jem.binarymodelingdata.BMDHeader#getSourceType()}.
+	 */
+	@Test
+	public final void testGetSourceType()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link gov.usgs.jem.binarymodelingdata.BMDHeader#getSpaces()}.
+	 */
+	@Test
+	public final void testGetSpaces()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link gov.usgs.jem.binarymodelingdata.BMDHeader#getStartTime()}.
+	 */
+	@Test
+	public final void testGetStartTime()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link gov.usgs.jem.binarymodelingdata.BMDHeader#getTimesSize()}.
+	 */
+	@Test
+	public final void testGetTimesSize()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link gov.usgs.jem.binarymodelingdata.BMDHeader#getVariablesSize()}.
+	 */
+	@Test
+	public final void testGetVariablesSize()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link gov.usgs.jem.binarymodelingdata.BMDHeader#getVersion()}.
+	 */
+	@Test
+	public final void testGetVersion()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link gov.usgs.jem.binarymodelingdata.BMDHeader#toString()}.
+	 */
+	@Test
+	public final void testToString()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+}
