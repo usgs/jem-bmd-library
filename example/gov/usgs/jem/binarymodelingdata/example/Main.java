4422068038db38435a8b8a3a654f695f04d07175
Mark McKelvy <mckelvym@usgs.gov>
2016-08-19 13:54:25 -0400

    Updated main example.

--- example/gov/usgs/jem/binarymodelingdata/example/Main.java
+++ example/gov/usgs/jem/binarymodelingdata/example/Main.java
@@ -1,11 +1,21 @@
 package gov.usgs.jem.binarymodelingdata.example;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.log4j.BasicConfigurator;
 
+import com.google.common.collect.Iterables;
+
 import gov.usgs.jem.binarymodelingdata.AllTests;
+import gov.usgs.jem.binarymodelingdata.BMDSegment;
+import gov.usgs.jem.binarymodelingdata.BMDTimeStep;
+import gov.usgs.jem.binarymodelingdata.BMDVariable;
+import gov.usgs.jem.binarymodelingdata.Concentration;
 import gov.usgs.jem.binarymodelingdata.input.BMDReader;
+import gov.usgs.jem.binarymodelingdata.input.ConcentrationsQuery;
 
 /**
  * Example using {@link BMDReader}
@@ -16,6 +26,12 @@ import gov.usgs.jem.binarymodelingdata.input.BMDReader;
  */
 public final class Main
 {
+	/**
+	 * Class logger
+	 */
+	private static final org.apache.log4j.Logger log = org.apache.log4j.Logger
+			.getLogger(Main.class);
+
 	public static void main(final String[] args) throws IOException
 	{
 		BasicConfigurator.configure();
@@ -23,6 +39,35 @@ public final class Main
 		try (BMDReader reader = BMDReader
 				.openDebug(AllTests.getTestFile().getAbsolutePath());)
 		{
+			final List<BMDVariable> variables = reader.getVariables();
+			log.info(String.format("Variables (%s): %s", variables.size(),
+					variables.stream().map(BMDVariable::getName)
+							.collect(Collectors.joining(", "))));
+			final List<BMDSegment> segments = reader.getSegments();
+			log.info(String.format("Segments (%s): %s", segments.size(),
+					segments.stream().map(BMDSegment::getName)
+							.collect(Collectors.joining(", "))));
+			final List<BMDTimeStep> timeSteps = reader.getTimeSteps();
+			log.info(String.format("Timesteps (%s): %s to %s", timeSteps.size(),
+					new java.util.Date(timeSteps.get(0).getTime()),
+					new java.util.Date(
+							Iterables.getLast(timeSteps).getTime())));
+
+			final ConcentrationsQuery query = reader.newConcentrationsQuery()
+					.withAllTimeSteps()
+					.withSegments(Arrays.asList(segments.get(0)))
+					.withVariables(variables.stream()
+							.filter(x -> x.getName().equals("Hydraulic Depth"))
+							.collect(Collectors.toList()));
+
+			for (final Concentration conc : query.execute())
+			{
+				log.info(String.format("%s, %s, %s: %s",
+						conc.getVariable().getName(),
+						conc.getSegment().getName(),
+						new java.util.Date(conc.getTimeStep().getTime()),
+						conc.getValue()));
+			}
 		}
 		catch (final Throwable t)
 		{
